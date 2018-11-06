bbb4edbab87efa541476df8a3099aa790a37790c
kaitou_ryaku <34281612+kaitou-ryaku@users.noreply.github.com>
2018-11-06 17:58:03 +0900

    BNF等のファイル読み込みをincludeからfopenに変更。ソース中の改行文字も読めるようになった

--- Makefile
+++ Makefile
@@ -8,7 +8,6 @@ TARGET  := compiler.out
 
 ALL_CH  := $(wildcard src/*.c include/*.h)
 ALL_O   := $(patsubst src/%.c,object/%.o,$(wildcard src/*.c))
-ALL_BNF := $(wildcard bnf/*.bnf bnf/*.txt)
 ALL_CH_WITHOUT_MAIN := $(filter-out src/main.c,$(ALL_CH))
 ALL_O_WITHOUT_MAIN  := $(filter-out object/main.o,$(ALL_O))
 
@@ -17,7 +16,7 @@ ALL_PARSER_CH := $(filter-out min-bnf-parser/src/main.c,$(wildcard min-bnf-parse
 
 dummy: $(TARGET)
 
-$(ALL_O_WITHOUT_MAIN): $(ALL_CH_WITHOUT_MAIN) $(ALL_BNF)
+$(ALL_O_WITHOUT_MAIN): $(ALL_CH_WITHOUT_MAIN)
 	cd object && $(MAKE) $(patsubst object/%.o,%.o,$@) "CC=$(CC)" "CFLAGS=$(CFLAGS)"
 
 $(ARCHIVE): $(ALL_O_WITHOUT_MAIN)
@@ -29,7 +28,7 @@ $(PARSER): $(ALL_PARSER_CH)
 $(REGEX):  $(ALL_REGEX_CH)
 	cd min-bnf-parser/min-regex && $(MAKE) $(patsubst min-bnf-parser/min-regex/%.a,%.a,$@) "CC=$(CC)" "CFLAGS=$(CFLAGS)"
 
-$(TARGET): $(ARCHIVE) $(PARSER) $(REGEX) $(ALL_BNF) src/main.c
+$(TARGET): $(ARCHIVE) $(PARSER) $(REGEX) src/main.c
 	cd object && $(MAKE) main.o "CC=$(CC)" "CFLAGS=$(CFLAGS)"
 	$(CC) $(CFLAGS) object/main.o $(ARCHIVE) $(PARSER) $(REGEX) -o $@
 
--- src/main.c
+++ src/main.c
@@ -5,28 +5,25 @@
 #include "../include/symbol.h"
 #include <stdio.h>
 
+static void read_file(const char* filename, char* str, const int str_max_size);
 int main(void) {
 
-  const char* lex_str = ""
-    #include "../bnf/mod_c_lex.bnf"
-  ;
-  const char* syntax_str = ""
-    #include "../bnf/mod_c_syntax.bnf"
-  ;
-  const char* pair_str = ""
-    #include "../bnf/mod_c_pair.txt"
-  ;
-  const char* src_str = ""
-    #include "../bnf/mod_c_source.txt"
-  ;
+  static char lex_str[100000];
+  static char syntax_str[100000];
+  static char pair_str[1000];
+  static char source_str[100000];
+  read_file("bnf/mod_c_lex.bnf"   , lex_str   , sizeof(lex_str)    / sizeof(char));
+  read_file("bnf/mod_c_syntax.bnf", syntax_str, sizeof(syntax_str) / sizeof(char));
+  read_file("bnf/mod_c_pair.txt"  , pair_str  , sizeof(pair_str)   / sizeof(char));
+  read_file("bnf/mod_c_source.txt", source_str, sizeof(source_str) / sizeof(char));
 
   BNF bnf[255]; // char:255文字のアルファベットに対応
   initialize_bnf(bnf, sizeof(bnf)/sizeof(BNF));
 
-  char           lex_name[2000];
-  char           lex_def[2000];
-  char           lex_simple[2000];
-  MIN_REGEX_NODE lex_node[2000];
+  static char           lex_name[50000];
+  static char           lex_def[50000];
+  static char           lex_simple[50000];
+  static MIN_REGEX_NODE lex_node[50000];
 
   const int lex_size = create_lex(
     lex_str, bnf
@@ -36,11 +33,11 @@ int main(void) {
     , lex_node   , sizeof(lex_node)   / sizeof(MIN_REGEX_NODE)
   );
 
-  char           work[2000];
-  char           syntax_name[4000];
-  char           syntax_def[20000];
-  char           syntax_simple[20000];
-  MIN_REGEX_NODE syntax_node[2000];
+  static char           work[20000];
+  static char           syntax_name[40000];
+  static char           syntax_def[200000];
+  static char           syntax_simple[200000];
+  static MIN_REGEX_NODE syntax_node[20000];
 
   const int syntax_size = create_syntax(
     syntax_str, bnf
@@ -58,13 +55,13 @@ int main(void) {
     FILE *fp;
     char *filename = "syntax.dot";
     if ((fp = fopen(filename, "w")) == NULL) {
-      fprintf(stderr, "Failed to open %s.\n", filename);
+      fprintf(stderr, "Error: Failed to open %s\n", filename);
     }
     syntax_to_dot(fp, bnf, "12.0", "0.2", "#FF0000", "#FF0000", "#000000");
     fclose(fp);
   }
 
-  PAIR_BNF pair_bnf[100];
+  static PAIR_BNF pair_bnf[1000];
 
   const int pair_size = create_pair_bnf(
     pair_str, bnf
@@ -74,25 +71,25 @@ int main(void) {
   fprintf(stderr, "pair size : %d\n", pair_size);
   print_pair_bnf(stderr, pair_bnf, bnf);
 
-  LEX_TOKEN token[1000];
-  const int token_size = match_lexer(token, sizeof(token)/sizeof(LEX_TOKEN), bnf, src_str);
+  static LEX_TOKEN token[1000];
+  const int token_size = match_lexer(token, sizeof(token)/sizeof(LEX_TOKEN), bnf, source_str);
   fprintf(stderr, "TOTAL TOKEN SIZE:%d\n", token_size);
   // print_token(stderr, bnf, token, token_size);
 
-  BLOCK block[1000];
+  static BLOCK block[10000];
   create_block(block, sizeof(block)/sizeof(BLOCK), token);
   print_block(stderr, block, token);
 
-  PARSE_TREE pt[5000];
-  static PARSE_MEMO memo[1000000];
+  static PARSE_TREE pt[500000];
+  static PARSE_MEMO memo[2000000]; // これがメモリを一番食う
   const int pt_size = parse_token_list(token, bnf, pair_bnf, pt, sizeof(pt)/sizeof(PARSE_TREE), memo, sizeof(memo)/sizeof(PARSE_MEMO));
 
   fprintf(stderr, "TOTAL PARSE TREE STEP:%d\n", pt_size);
   print_parse_tree(stderr, pt_size, pt, bnf, token);
 
-  //配列に未対応なので一旦コメントアウト
-  //SYMBOL symbol[1000];
-  //create_symbol_table(block, token, bnf, pt, symbol, sizeof(symbol)/sizeof(SYMBOL));
+  // 配列に未対応なので一旦コメントアウト
+  // static SYMBOL symbol[10000];
+  // create_symbol_table(block, token, bnf, pt, symbol, sizeof(symbol)/sizeof(SYMBOL));
 
   translate_pt_to_ast(pt, bnf);
   print_parse_tree(stderr, pt_size, pt, bnf, token);
@@ -101,7 +98,7 @@ int main(void) {
     FILE *fp;
     char *filename = "parse_tree.dot";
     if ((fp = fopen(filename, "w")) == NULL) {
-      fprintf(stderr, "Failed to open %s.\n", filename);
+      fprintf(stderr, "Error: Failed to open %s\n", filename);
     }
 
     origin_parse_tree_to_dot(fp, 0, pt, bnf, token, "12.0", NULL, "#FF0000", "#000000");
@@ -111,3 +108,24 @@ int main(void) {
 
   return 0;
 }
+
+static void read_file(const char* filename, char* str, const int str_max_size) {/*{{{*/
+  FILE *fp;
+  if ((fp = fopen(filename, "r")) == NULL) {
+    fprintf(stderr, "Error: Failed to open %s\n", filename);
+
+  } else {
+    int ch;
+    int i=0;
+    while ((ch = fgetc(fp)) != EOF) {
+      str[i] = (char) ch;
+      i++;
+
+      if (i >= str_max_size) {
+        fprintf(stderr, "Error: Too Large File:%s\n", filename);
+        break;
+      }
+    }
+  }
+  fclose(fp);
+}/*}}}*/
