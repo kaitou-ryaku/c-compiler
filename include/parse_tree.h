29606fb3267f6b67befe550f2129614b8bf9a559
kaitou_ryaku <34281612+kaitou-ryaku@users.noreply.github.com>
2018-11-09 02:03:50 +0900

    不要なファイルを削除

--- include/parse_tree.h
+++
@@ -1,6 +0,0 @@
-#ifndef __C_COMPILER_PARSE_TREE__
-#define __C_COMPILER_PARSE_TREE__
-
-void parse_tree(void);
-
-#endif
--- src/parse_tree.c
+++
@@ -1,7 +0,0 @@
-#include "../min-bnf-parser/include/min-bnf-parser.h"
-#include "../include/parse_tree.h"
-#include <stdio.h>
-
-extern void parse_tree(void) {
-  return;
-}
