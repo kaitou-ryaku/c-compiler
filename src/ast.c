5f191b5a56a924385be38f2529979bbb2ac77a36
kaitou-ryaku <kaitou-ryaku@users.noreply.github.com>
2018-11-22 03:59:46 +0900

    POSTFIX_EXPRESSIONを二分木に変換

--- src/ast.c
+++ src/ast.c
@@ -14,12 +14,15 @@ static bool delete_syntax_symbol_recursive(const int top, PARSE_TREE* pt, const 
 static void delete_syntax_symbol(PARSE_TREE* pt, const BNF* bnf);
 static bool two_operatir_to_binary_tree_recursive(const int top, PARSE_TREE* pt, const BNF* bnf);
 static void two_operatir_to_binary_tree(PARSE_TREE* pt, const BNF* bnf);
+static bool postfix_to_binary_tree_recursive(const int top, PARSE_TREE* pt, const BNF* bnf);
+static void postfix_to_binary_tree(PARSE_TREE* pt, const BNF* bnf);
 /*}}}*/
 
 extern void translate_pt_to_ast(PARSE_TREE* pt, const BNF* bnf) {/*{{{*/
   delete_syntax_symbol(pt, bnf);
   delete_solitary_container(pt, bnf);
   two_operatir_to_binary_tree(pt, bnf);
+  postfix_to_binary_tree(pt, bnf);
 }/*}}}*/
 static bool delete_lift_solitary_pt(const int index, PARSE_TREE* pt) {/*{{{*/
   // 削除したらtrue, 削除しなかったらfalseを返す
@@ -241,4 +244,90 @@ static bool two_operatir_to_binary_tree_recursive(const int top, PARSE_TREE* pt,
 static void two_operatir_to_binary_tree(PARSE_TREE* pt, const BNF* bnf) {/*{{{*/
   bool is_change = true;
   while (is_change) is_change = two_operatir_to_binary_tree_recursive(0, pt, bnf);
+}/*}}}*/
+static bool postfix_to_binary_tree_recursive(const int top, PARSE_TREE* pt, const BNF* bnf) {/*{{{*/
+  bool is_change = false;
+  if (is_pt_name("POSTFIX_EXPRESSION", pt[top], bnf)) {
+    assert(pt[top].down >= 0);
+
+    const int down = pt[top].down;
+    const int right = pt[down].right;
+
+    if ((right >= 0) && (is_pt_name("dot", pt[right], bnf) || is_pt_name("minus_rangle", pt[right], bnf))) {
+      // a . b [c] を .(a,b)  c に変更
+
+      is_change = true;
+      assert(right >= 0 && pt[right].right >= 0);
+
+      is_change = true;
+      const int fst = pt[top].down;
+      const int ope = pt[fst].right;
+      const int snd = pt[ope].right;
+
+      assert(is_lex(bnf[pt[ope].bnf_id]));
+
+      pt[top].down = ope;
+      pt[ope].up   = top;
+      pt[ope].left  = pt[fst].left; assert(pt[ope].left < 0);
+      pt[ope].right = pt[snd].right;
+      pt[ope].down  = fst;
+
+      pt[fst].left  = -1;
+      pt[fst].right = snd;
+      pt[fst].up    = ope;
+
+      pt[snd].left  = fst;
+      pt[snd].right = -1;
+      pt[snd].up    = ope;
+
+      postfix_to_binary_tree_recursive(fst, pt, bnf);
+      postfix_to_binary_tree_recursive(snd, pt, bnf);
+      if (pt[ope].right >= 0) postfix_to_binary_tree_recursive(pt[ope].right, pt, bnf);
+
+    } else if (right >= 0 && pt[right].right >= 0) {
+      // a [b] . c を PE(a,b) . c に変更
+
+      is_change = true;
+      assert(pt[0].used_size+1 < pt[0].total_size);
+      const int additional = pt[0].used_size;
+      pt[0].used_size++;
+
+      pt[additional].state             = pt[top].state;
+      pt[additional].bnf_id            = pt[top].bnf_id;
+      pt[additional].up_bnf_node_index = pt[top].up_bnf_node_index;
+      pt[additional].token_begin_index = pt[down].token_begin_index;
+      pt[additional].token_end_index   = pt[right].token_end_index;
+
+      pt[top].down        = additional;
+
+      pt[additional].up    = top;
+      pt[additional].down  = down;
+      pt[additional].left  = -1;;
+      pt[additional].right = pt[right].right;
+
+      pt[down].up = additional;
+      assert(pt[down].left < 0);
+      assert(pt[down].right == right);
+
+      pt[right].up   = additional;
+      //assert(pt[right].left == down);
+      pt[right].left = down;
+      pt[right].right = -1;
+
+      pt[pt[right].right].left = additional;
+
+      postfix_to_binary_tree_recursive(down, pt, bnf);
+      postfix_to_binary_tree_recursive(right, pt, bnf);
+      postfix_to_binary_tree_recursive(pt[additional].right, pt, bnf);
+    }
+  }
+
+  if (!is_change && (pt[top].down  >= 0)) is_change = postfix_to_binary_tree_recursive(pt[top].down, pt, bnf);
+  if (!is_change && (pt[top].right >= 0)) is_change = postfix_to_binary_tree_recursive(pt[top].right, pt, bnf);
+
+  return is_change;
+}/*}}}*/
+static void postfix_to_binary_tree(PARSE_TREE* pt, const BNF* bnf) {/*{{{*/
+  bool is_change = true;
+  while (is_change) is_change = postfix_to_binary_tree_recursive(0, pt, bnf);
 }/*}}}*/
